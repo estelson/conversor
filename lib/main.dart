54d963d95539d155c5a5204f07e9b26bc9fd6633
Estelson Medeiros Pereira <estelson@gmail.com>
2022-11-06 10:52:28 -0300

    Requisitando dados ao webservice via json.decode

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:async/async.dart';
+import 'dart:convert';
 
 const request = "https://api.hgbrasil.com/finance?format=json&key=dc7fe941";
 
 void main() async {
 
   http.Response response = await http.get(Uri.parse(request));
-  print(response);
+  print(json.decode(response.body)["results"]["currencies"]["USD"]);
 
   runApp(MaterialApp(
     home: Container()
