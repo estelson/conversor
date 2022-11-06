eebd26cda22d2cb58811bda8346758035b121855
Estelson Medeiros Pereira <estelson@gmail.com>
2022-11-06 11:05:34 -0300

    Criar a interface do APP

--- lib/main.dart
+++ lib/main.dart
@@ -5,13 +5,34 @@ import 'dart:convert';
 
 const request = "https://api.hgbrasil.com/finance?format=json&key=dc7fe941";
 
-void main() async {
-  runApp(MaterialApp(
-    home: Container()
-  ));
-}
-
 Future<Map> getData() async {
   http.Response response = await http.get(Uri.parse(request));
   return json.decode(response.body);
+}
+
+void main() async {
+  runApp(const MaterialApp(
+    home: Home(),
+  ));
+}
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        title: const Text("\$\$ Conversor de moedas \$\$"),
+        backgroundColor: Colors.amber,
+        centerTitle: true,
+      ),
+    );
+  }
 }
